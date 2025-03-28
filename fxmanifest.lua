347af428e14c7a32cfeffb5e893804d3dd2c9901
OldWestCreations <m.wollgramm90@gmail.com>
2025-03-28 07:34:34 +0000

    added versioncheck

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,3 +13,8 @@ server_script 'config.lua'
 server_script 'server.lua'
 
 escrow_ignore 'config.lua'
+
+version '1.0.0'
+vorp_checker 'yes'
+vorp_name '^owc_exchange ^4version Check^3' 
+vorp_github 'https://github.com/OldWestCreations/owc_exchange' 
