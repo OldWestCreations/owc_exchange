245ad588221a8b0fb919ec59a773cf04f5f027d8
OldWestCreations <m.wollgramm90@gmail.com>
2025-03-28 10:26:03 +0000

    testing version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,14 @@
-game 'rdr3'
-fx_version 'adamant'
+author 'Real Stoner Gamer '
+fx_version "adamant"
+version '1.3'
+games {'rdr3'}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-lua54 'yes'
 
-author '@HerrScaletta | Old West Creations'
-description 'Simple currency exchange script for VorpCore RedM'
+client_scripts {
+    'client.lua',
+    'config.lua'
+} 
 
-client_script 'config.lua'
-client_script 'client.lua'
-
-server_script 'config.lua'
-server_script 'server.lua'
-
-escrow_ignore 'config.lua'
-
-version '1.0.0'
 vorp_checker 'yes'
-vorp_name '^owc_exchange ^4version Check^3' 
-vorp_github 'https://github.com/OldWestCreations/owc_exchange' 
+vorp_name '^5 OldWestCreation ^4version Check^3'
+vorp_github 'https://github.com/RealStonerGamer/RSG_RemoveAnyPed'
